fecd156bdf8b749a7e5cc9f9ef18f402d5549715
havansang <havansang090203@gmail.com>
2023-12-13 11:11:32 +0700

    test

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Bai_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            ThisinhA ts = new ThisinhA();
+
+
+            Console.ReadKey();
+        }
+    }
+}
